25b2dd6a28fd01845811c7493cfe83f6312cd5a7
Mukul001 <mukulaggarwal1996@gmail.com>
2022-12-29 22:51:52 +0530

    server setup

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log("Start")
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', function (req, res, next) {
+    console.log(req.protocol + '://' + req.get('host') + req.originalUrl);
+    next();
+});
+
+try {
+    mongoose.set('strictQuery', false);
+    mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Database connected");
+      })
+      .catch((err) => console.log(`Database connection error: ${err.message}`));
+
+} catch (error) {
+    handleError(error);
+}
+
+app.listen(process.env.APP_PORT, () => {
+    console.log(`Server connect on port number ${process.env.APP_PORT}`);
+})
